419c2675e57433d03a089acce6aee2f9121008ba
Flash <50988818+etoFlash@users.noreply.github.com>
2020-01-19 01:59:11 +0300

    Add files via upload

--- 057-063/lab_05.py
+++ 057-063/lab_05.py
@@ -15,11 +15,16 @@ def median(lst):
         return (sorted_list[median_index - 1] + sorted_list[median_index]) / 2
 
 
+def count_unique(lst):
+    return len(set(lst))
+
+
 if __name__ == '__main__':
     with open("lab_05.txt", "r") as f:
         temperatures = list(map(float, f.readlines()))
     print("max:", max(temperatures))
     print("min:", min(temperatures))
+    print("count_unique:", count_unique(temperatures))
     print("mean:", mean(temperatures))
     print("median:", median(temperatures))
     print("statistics.mean:", statistics.mean(temperatures))
