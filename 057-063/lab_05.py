a9e5e4308ac4172ccc7e2ff0bb7a7511e5f680dd
Flash <50988818+etoFlash@users.noreply.github.com>
2020-01-19 01:55:04 +0300

    Add files via upload

---
+++ 057-063/lab_05.py
@@ -0,0 +1,26 @@
+import statistics
+
+
+def mean(lst):
+    return sum(lst) / len(lst)
+
+
+def median(lst):
+    sorted_list = sorted(lst)
+    list_len = len(sorted_list)
+    median_index = list_len // 2
+    if list_len % 2 == 1:
+        return sorted_list[median_index]
+    else:
+        return (sorted_list[median_index - 1] + sorted_list[median_index]) / 2
+
+
+if __name__ == '__main__':
+    with open("lab_05.txt", "r") as f:
+        temperatures = list(map(float, f.readlines()))
+    print("max:", max(temperatures))
+    print("min:", min(temperatures))
+    print("mean:", mean(temperatures))
+    print("median:", median(temperatures))
+    print("statistics.mean:", statistics.mean(temperatures))
+    print("statistics.median:", statistics.median(temperatures))
