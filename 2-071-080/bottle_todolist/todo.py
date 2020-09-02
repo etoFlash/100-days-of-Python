d8de4d3cdc308040a79d6b8b3e66fd464eb336f9
etoFlash <flashback.rce@gmail.com>
2020-09-02 13:00:57 +0300

    Add Day 78

--- 2-071-080/bottle_todolist/todo.py
+++ 2-071-080/bottle_todolist/todo.py
@@ -1,6 +1,8 @@
 import sqlite3
+import csv
+from io import StringIO
 
-from bottle import get, post, request, template, run, static_file
+from bottle import get, post, request, template, run, static_file, response
 import aiosql
 
 
@@ -8,44 +10,48 @@ conn = sqlite3.connect("todo.db")
 queries = aiosql.from_path("tasks.sql", "sqlite3")
 
 
-@get(f"/static/js/<filename>")
+@get("/static/js/<filename>")
 def static_js(filename):
     return static_file(filename, root="static/js")
 
 
 @get("/")
 @post("/")
-def home():
+def index():
     if request.method == "POST":
         task_text = request.forms.get('taskInput')
         add_task(task_text)
 
-    return template('templates/index.html', tasks=queries.get_tasks(conn))
+    return template('templates/index.html', tasks=get_tasks())
 
 
 @post("/delete/<id_task:int>")
 def delete(id_task):
-    order_num = queries.get_order_task(conn, id_task=id_task)[0]
-
-    queries.delete_task(conn, id_task=id_task)
-    queries.reorder_tasks(conn, order_num=order_num)
-
-    conn.commit()
+    delete_task(id_task)
 
 
 @post("/move/<id_task:int>/<direction:re:(up|down)>")
 def move(id_task, direction):
-    order_num = queries.get_order_task(conn, id_task=id_task)[0]
-
-    if not order_num:
-        return
-
-    if direction == 'up' and order_num < queries.get_max_order(conn)[0]:
-        queries.switch_order_nums(conn, order_num1=order_num, order_num2=order_num + 1)
-    elif direction == 'down' and order_num > 1:
-        queries.switch_order_nums(conn, order_num1=order_num, order_num2=order_num - 1)
+    move_task(direction, id_task)
 
     return template('templates/tasks.html', tasks=queries.get_tasks(conn))
+
+
+@get("/download")
+def download():
+    return download_tasks()
+
+
+def get_tasks():
+    return queries.get_tasks(conn)
+
+
+def delete_task(id_task):
+    order_num = queries.get_order_task(conn, id_task=id_task)[0]
+    queries.delete_task(conn, id_task=id_task)
+    queries.reorder_tasks(conn, order_num=order_num)
+
+    conn.commit()
 
 
 def add_task(task_text):
@@ -53,6 +59,28 @@ def add_task(task_text):
     queries.add_task(conn, task_text=task_text, order_num=order_num)
 
     conn.commit()
+
+
+def move_task(direction, id_task):
+    order_num = queries.get_order_task(conn, id_task=id_task)[0]
+    assert order_num, "Order number not found"
+
+    if direction == 'up' and order_num < queries.get_max_order(conn)[0]:
+        queries.switch_order_nums(conn, order_num1=order_num, order_num2=order_num + 1)
+    elif direction == 'down' and order_num > 1:
+        queries.switch_order_nums(conn, order_num1=order_num, order_num2=order_num - 1)
+
+
+def download_tasks():
+    stream = StringIO()
+    writer = csv.writer(stream)
+    writer.writerow(["id_task", "task_text", "order_num"])
+    writer.writerows(queries.get_tasks(conn))
+
+    response.set_header("Content-type", "text/csv")
+    response.set_header("Content-Disposition", "attachment; filename=tasks.csv")
+
+    return stream.getvalue().encode("shift-jis")
 
 
 if __name__ == '__main__':
